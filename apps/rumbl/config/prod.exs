70e195456c681163bac2bab3ab17163fe86b24a6
Giovanni Francischelli <gifrancischelli@gmail.com>
2017-10-12 19:06:00 -0300

    Add deploy config with gatling and distillery

--- apps/rumbl/config/prod.exs
+++ apps/rumbl/config/prod.exs
@@ -13,8 +13,12 @@ use Mix.Config
 # which you typically run after static files are built.
 config :rumbl, Rumbl.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [host: "localhost", port: {:system, "PORT"}],
+  cache_static_manifest: "priv/static/manifest.json",
+  server: true,
+  root: ".",
+  version: Application.spec(:myapp, :vsn)
+
 
 # Do not print debug messages in production
 config :logger, level: :info
