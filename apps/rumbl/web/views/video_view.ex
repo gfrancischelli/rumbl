eebc2d3e3f17915eb6b48316a7f641cddb6c834e
Giovanni Francischelli <gifrancischelli@gmail.com>
2017-10-15 23:30:56 -0200

    :lipstick: Change videos list from table to flex container with Bootstrap Cards

--- apps/rumbl/web/views/video_view.ex
+++ apps/rumbl/web/views/video_view.ex
@@ -1,3 +1,22 @@
 defmodule Rumbl.VideoView do
   use Rumbl.Web, :view
+
+  def video_thumbnail(%Rumbl.Video{url: url}) do
+    # "https://www.youtube.com/watch?v=" <> youtube_id = url
+    # "https://img.youtube.com/vi/" <> youtube_id <> "/mqdefault.jpg"
+    case url do
+      "https://www.youtube.com/watch?v=" <> youtube_id ->
+        "https://img.youtube.com/vi/" <> youtube_id <> "/mqdefault.jpg"
+      _ ->
+        ""
+    end
+  end
+
+  def render_desc(%Rumbl.Video{description: desc}) do
+    if String.length(desc) > 140 do
+      String.slice(desc, 0..140) <> "..."
+    else
+      desc
+    end
+  end
 end
